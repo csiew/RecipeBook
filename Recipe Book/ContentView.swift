43ac17522aa95207bc3dc4f97407fea8ff480854
csiew <clarence.siew@gmail.com>
2020-06-03 02:20:12 +1000

    Initial Commit

---
+++ Recipe Book/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Recipe Book
+//
+//  Created by Clarence Siew on 3/6/20.
+//  Copyright © 2020 Clarence Siew. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
